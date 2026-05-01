c046c93397476e7d8345ae0c78f062f85689b7f5
Guillermo <guillermogomezmorillas@gmail.com>
2026-05-01 02:25:18 +0200

    ajustes js

--- js/animations.js
+++ js/animations.js
@@ -30,15 +30,35 @@ gsap.from(".imagen-secundaria", {
   ease: "power3.out"
 });
 
-gsap.utils.toArray(".portada-seccion").forEach((img) => {
-  gsap.from(img, {
-    scrollTrigger: {
-      trigger: img,
-      start: "top 85%",
-    },
-    x: -300,
-    opacity: 0,
-    duration: 1,
-    ease: "power4.out"
+
+let mm = gsap.matchMedia();
+
+mm.add("(max-width: 768px)", () => {
+  // 📱 MÓVIL
+  gsap.utils.toArray(".portada-seccion").forEach((img) => {
+    gsap.from(img, {
+      scrollTrigger: {
+        trigger: img,
+        start: "top 95%", // 👈 SOLO en móvil
+      },
+      y: 50,
+      opacity: 0,
+      duration: 0.6
+    });
+  });
+});
+
+mm.add("(min-width: 769px)", () => {
+  // 💻 DESKTOP
+  gsap.utils.toArray(".portada-seccion").forEach((img) => {
+    gsap.from(img, {
+      scrollTrigger: {
+        trigger: img,
+        start: "top 80%", // 👈 escritorio
+      },
+      y: 100,
+      opacity: 0,
+      duration: 1
+    });
   });
 });
